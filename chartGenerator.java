f57217a58244353e4ef30c987001a4c093858803
xanderle <x.marks.the.spot.xd@gmail.com>
2016-10-13 09:56:07 +0800

    Manually Enter domain format
    Manually enter domain format dd/MM/yyyy
    lower than upper
    need to fix invalid error

--- chartGenerator.java
+++ chartGenerator.java
@@ -1,23 +1,37 @@
+package cits3200;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.text.DateFormat;
 import java.text.NumberFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import javax.swing.AbstractAction;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
@@ -25,7 +39,7 @@ import org.jfree.chart.axis.DateAxis;
 import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
-import org.jfree.data.general.Dataset;
+import org.jfree.data.time.Day;
 import org.jfree.data.time.Minute;
 import org.jfree.data.time.TimeSeries;
 import org.jfree.data.time.TimeSeriesCollection;
@@ -35,30 +49,31 @@ import org.jfree.ui.ApplicationFrame;
 
 
 public class chartGenerator extends ApplicationFrame {
-
     private static final String title = "Versatile Analysis of Body Temperature";
-    private ChartPanel chartPanel;
-    private static DataSet dset;
-
-    public chartGenerator(DataSet data) {
-        super("Temperature Analysis");
+    public ChartPanel chartPanel;
+    private TimeSeriesCollection roiData = new TimeSeriesCollection( );
+   
+    public chartGenerator(DataSet dset) {
+    	super("Temperature Analysis");
+    	chartPanel = createChart(dset);
     	JFrame f = new JFrame(title);
         f.setTitle(title);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setLayout(new BorderLayout(0, 5));
-        chartPanel = createChart(data);
         f.add(chartPanel, BorderLayout.CENTER);
         JPanel panel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
         panel.add(createZoomOut());
         panel.add(createZoomIn());
+        panel.add(lowerBound());
+        panel.add(upperBound());
         f.add(panel, BorderLayout.SOUTH);
         f.pack();
         f.setLocationRelativeTo(null);
         f.setVisible(true);
     }
     
-    private ChartPanel createChart(DataSet data) {
-        TimeSeriesCollection roiData = createDataset(data);
+    private ChartPanel createChart(DataSet dset) {
+        roiData = createDataset(dset);
         
         JFreeChart chart = ChartFactory.createTimeSeriesChart(
             title, "Date", "Temperature", roiData, true, true, false);
@@ -68,113 +83,34 @@ public class chartGenerator extends ApplicationFrame {
         plot.setRangePannable(true);
         XYLineAndShapeRenderer renderer =
             (XYLineAndShapeRenderer) plot.getRenderer();
-        
         NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
         return new ChartPanel(chart);
     }
     
-    private static TimeSeriesCollection createDataset(DataSet data){
-	     final TimeSeries sheep = new TimeSeries("Sheep");
-	     
-/*---------------------old file reading code-------------------------------------
-	     File file = new File("data.csv");
-	     FileInputStream fis = null;
-	     BufferedInputStream bis = null;
-	     DataInputStream dis = null;
-	     try{
-	       fis = new FileInputStream(file);
-	       bis = new BufferedInputStream(fis);
-	       dis = new DataInputStream(bis);
-	       dis.readLine();
-	       int day;
-	       int month;
-	       int year;
-	       int hour;
-	       int minute;
-	       String[] parts;
-	       String[] dateTime;
-	       String dateString;
-	       String timeString;
-	       String[] date;
-	       String[] time;
-	       
-	     //initialise DataSet attributes
-	       int samplingRate;
-	       double[] data;
-	       double d;
-	       int oldHour = 0, newHour = 0 , oldMinute = 0, newMinute = 0;
-	       ArrayList<Double> datalist = new ArrayList<Double>();
+    class MenuActionListener implements ActionListener {
+  	  public void actionPerformed(ActionEvent e) {
+  	    System.out.println("Selected: " + e.getActionCommand());
 
-	       while(dis.available() != 0){
-	         String line = dis.readLine();
-	         parts = line.split(",");
-//	         System.out.println("Parts is "+parts[0]+" "+parts[1]);
-	         dateTime= parts[0].split(" ");
-//	         System.out.println("Date "+dateTime[0]+" Time "+dateTime[1]);
-	         dateString = dateTime[0];
-	         timeString = dateTime[1];
-	         date = dateString.split("/");
-	         time = timeString.split(":");
-	         day = Integer.parseInt(date[0]);
-	         month = Integer.parseInt(date[1]);
-	         year = Integer.parseInt(date[2]);
-	         hour = Integer.parseInt(time[0]);
-	         minute = Integer.parseInt(time[1]);
-
-	         System.out.println(day);
-	         System.out.println(month);
-	         System.out.println(year);
-	         System.out.println(hour);
-	         System.out.println(minute);
-
-	         d = Double.parseDouble(parts[1]);
-	         sheep.add(new Minute(minute,hour,day,month,year),d);
-	         datalist.add(d);
-	         
-	         //calculate sampling rate
-	         if ( (datalist.size() & 1) == 0) {
-	        	 oldHour = hour;
-	        	 oldMinute = minute;
-	         } else {
-	        	 newHour = hour;
-	        	 newMinute = minute;
-	         }
-	       }
-	       
-	       fis.close();
-	       bis.close();
-	       dis.close();
-	       
-	       samplingRate = newHour * 60 + newMinute - oldHour * 60 - oldMinute;
-	       data = new double[datalist.size()];
-	       for (int i = 0; i < datalist.size(); i++) {
-	    	   data[i] = datalist.get(i).doubleValue();
-	       }
-
-	       dset = new DataSet(samplingRate,datalist.size(),data);
-	     }
-
-	     catch(FileNotFoundException e){
-	       e.printStackTrace();
-	     }
-	     catch(IOException e){
-	       e.printStackTrace();
-	     }
---------------------------------------------------------------------------------------*/
-
-		 //initialise DataSet attributes
+  	  }
+  	}
+    private void addData(ArrayList<Minute> dates ,ArrayList<Integer> values){
+    	final TimeSeries analysis = new TimeSeries("Analysis");
+    	
+    	roiData.addSeries(analysis);
+    }
+    private static TimeSeriesCollection createDataset(DataSet dset){
+	     final TimeSeries series = new TimeSeries("Series");
 		 double d;
 		 ArrayList<Double> datalist = new ArrayList<Double>();
-		 int N = data.N;
+		 int N = dset.N;
 		 for(int i=0; i<N; i++)
 		 {
-		   d = data.values[i];
-		   sheep.addOrUpdate(new Minute(data.times[i]),d);
+		   d = dset.values[i];
+		   series.addOrUpdate(new Minute(dset.times[i]),d);
 		   datalist.add(d);
 		 }
-		 dset = data;
 	     final TimeSeriesCollection dataset = new TimeSeriesCollection( );
-	     dataset.addSeries(sheep);
+	     dataset.addSeries(series);
 	     return dataset;
   }
     
@@ -204,18 +140,70 @@ public class chartGenerator extends ApplicationFrame {
         return auto;
     }
     
-    public DataSet getDataSet() {
- 	   return dset;
-    }
-/*    
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-
+    private JFormattedTextField lowerBound() {
+    	DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+    	JFormattedTextField lowerBound = new JFormattedTextField(df);
+    	lowerBound.setColumns(10);
+    	lowerBound.addKeyListener(new KeyAdapter() {
+    	    public void keyTyped(KeyEvent e) {
+    	      char c = e.getKeyChar();
+    	      if (!((c >= '0') && (c <= '9') ||
+    	         (c == KeyEvent.VK_BACK_SPACE) ||
+    	         (c == KeyEvent.VK_DELETE) || (c == KeyEvent.VK_SLASH)))        
+    	      {
+    	        JOptionPane.showMessageDialog(null, "Please Enter Valid");
+    	        e.consume();
+    	      }
+    	    }
+    	  });
+    	
+        lowerBound.addActionListener(new ActionListener(){
             @Override
-            public void run() {
-               chartGenerator cpd = new chartGenerator();
+            public void actionPerformed(ActionEvent e) {
+            	final double r2 = chartPanel.getChart().getXYPlot().getDomainAxis().getUpperBound();
+            	final double r1 = chartPanel.getChart().getXYPlot().getDomainAxis().getLowerBound();        	
+            	Date startDate = null;
+            	try {
+					startDate = df.parse(lowerBound.getText());
+				} catch (ParseException e1) {
+					e1.printStackTrace();
+				}
+    			chartPanel.getChart().getXYPlot().getDomainAxis().setRange((double) startDate.getTime(),r2);
             }
         });
+        return lowerBound;
     }
-*/
+    private JFormattedTextField upperBound() {
+    	DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+    	JFormattedTextField upperBound = new JFormattedTextField(df);
+    	upperBound.setColumns(10);
+    	upperBound.addKeyListener(new KeyAdapter() {
+    	    public void keyTyped(KeyEvent e) {
+    	      char c = e.getKeyChar();
+    	      if (!((c >= '0') && (c <= '9') ||
+    	         (c == KeyEvent.VK_BACK_SPACE) ||
+    	         (c == KeyEvent.VK_DELETE) || (c == KeyEvent.VK_SLASH)))        
+    	      {
+    	        JOptionPane.showMessageDialog(null, "Please Enter Valid");
+    	        e.consume();
+    	      }
+    	    }
+    	  });
+    	
+    	upperBound.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	final double r2 = chartPanel.getChart().getXYPlot().getDomainAxis().getUpperBound();
+            	final double r1 = chartPanel.getChart().getXYPlot().getDomainAxis().getLowerBound();        	
+            	Date startDate = null;
+            	try {
+					startDate = df.parse(upperBound.getText());
+				} catch (ParseException e1) {
+					e1.printStackTrace();
+				}            	
+    			chartPanel.getChart().getXYPlot().getDomainAxis().setRange(r1,(double) startDate.getTime());
+            }
+        });
+        return upperBound;
+    }
 }
