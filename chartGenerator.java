d25aed1cc522983c5247c988bd5c3ef8cd2b4d7e
Xue Yu <yuxue@yuxue.me>
2016-10-13 23:43:05 +0800

    removed package info and fixed an issue when closing the chart window will also close the program

--- chartGenerator.java
+++ chartGenerator.java
@@ -1,4 +1,3 @@
-package cits3200;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
@@ -52,13 +51,12 @@ public class chartGenerator extends ApplicationFrame {
     private static final String title = "Versatile Analysis of Body Temperature";
     public ChartPanel chartPanel;
     private TimeSeriesCollection roiData = new TimeSeriesCollection( );
-   
+
     public chartGenerator(DataSet dset) {
     	super("Temperature Analysis");
     	chartPanel = createChart(dset);
     	JFrame f = new JFrame(title);
         f.setTitle(title);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setLayout(new BorderLayout(0, 5));
         f.add(chartPanel, BorderLayout.CENTER);
         JPanel panel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
@@ -71,13 +69,13 @@ public class chartGenerator extends ApplicationFrame {
         f.setLocationRelativeTo(null);
         f.setVisible(true);
     }
-    
+
     private ChartPanel createChart(DataSet dset) {
         roiData = createDataset(dset);
-        
+
         JFreeChart chart = ChartFactory.createTimeSeriesChart(
             title, "Date", "Temperature", roiData, true, true, false);
-        
+
         XYPlot plot = chart.getXYPlot();
         plot.setDomainPannable(true);
         plot.setRangePannable(true);
@@ -86,7 +84,7 @@ public class chartGenerator extends ApplicationFrame {
         NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
         return new ChartPanel(chart);
     }
-    
+
     class MenuActionListener implements ActionListener {
   	  public void actionPerformed(ActionEvent e) {
   	    System.out.println("Selected: " + e.getActionCommand());
@@ -94,7 +92,7 @@ public class chartGenerator extends ApplicationFrame {
   	}
     private void addData(ArrayList<Minute> dates ,ArrayList<Integer> values){
     	final TimeSeries analysis = new TimeSeries("Analysis");
-    	
+
     	roiData.addSeries(analysis);
     }
     private static TimeSeriesCollection createDataset(DataSet dset){
@@ -112,7 +110,7 @@ public class chartGenerator extends ApplicationFrame {
 	     dataset.addSeries(series);
 	     return dataset;
   }
-    
+
     private JButton createZoomOut() {
         final JButton auto = new JButton(new AbstractAction("Zoom Out") {
             @Override
@@ -125,7 +123,7 @@ public class chartGenerator extends ApplicationFrame {
         });
         return auto;
     }
-    
+
     private JButton createZoomIn() {
         final JButton auto = new JButton(new AbstractAction("Zoom In") {
             @Override
@@ -138,7 +136,7 @@ public class chartGenerator extends ApplicationFrame {
         });
         return auto;
     }
-    
+
     private JFormattedTextField lowerBound() {
     	DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
     	JFormattedTextField lowerBound = new JFormattedTextField(df);
@@ -148,18 +146,18 @@ public class chartGenerator extends ApplicationFrame {
     	      char c = e.getKeyChar();
     	      if (!((c >= '0') && (c <= '9') ||
     	         (c == KeyEvent.VK_BACK_SPACE) ||
-    	         (c == KeyEvent.VK_DELETE) || (c == KeyEvent.VK_ENTER) ||(c == KeyEvent.VK_SLASH)))        
+    	         (c == KeyEvent.VK_DELETE) || (c == KeyEvent.VK_ENTER) ||(c == KeyEvent.VK_SLASH)))
     	      {
     	        JOptionPane.showMessageDialog(null, "Please Enter Valid");
     	        e.consume();
     	      }
     	    }
     	  });
-    	
+
         lowerBound.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
-            	final double r2 = chartPanel.getChart().getXYPlot().getDomainAxis().getUpperBound();        	
+            	final double r2 = chartPanel.getChart().getXYPlot().getDomainAxis().getUpperBound();
             	Date startDate = null;
             	try {
 					startDate = df.parse(lowerBound.getText());
@@ -172,7 +170,7 @@ public class chartGenerator extends ApplicationFrame {
             	else{
             		JOptionPane.showMessageDialog(null, "Lower Bound is Higher than Upper Bound");
             	}
-    			
+
     			lowerBound.setText("");
             }
         });
@@ -187,26 +185,26 @@ public class chartGenerator extends ApplicationFrame {
     	      char c = e.getKeyChar();
     	      if (!((c >= '0') && (c <= '9') ||
     	         (c == KeyEvent.VK_BACK_SPACE) ||
-    	         (c == KeyEvent.VK_DELETE) || (c == KeyEvent.VK_ENTER) || (c == KeyEvent.VK_SLASH)))        
+    	         (c == KeyEvent.VK_DELETE) || (c == KeyEvent.VK_ENTER) || (c == KeyEvent.VK_SLASH)))
     	      {
     	        JOptionPane.showMessageDialog(null, "Please Enter Valid");
     	        e.consume();
     	      }
     	    }
     	  });
-    	
+
     	upperBound.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
 
-            	final double r1 = chartPanel.getChart().getXYPlot().getDomainAxis().getLowerBound();        	
+            	final double r1 = chartPanel.getChart().getXYPlot().getDomainAxis().getLowerBound();
             	Date startDate = null;
             	try {
 					startDate = df.parse(upperBound.getText());
-					
+
 				} catch (ParseException e1) {
 					e1.printStackTrace();
-				}            	
+				}
             	if((double) startDate.getTime() > r1){
             		chartPanel.getChart().getXYPlot().getDomainAxis().setRange(r1,(double) startDate.getTime());
 				}
